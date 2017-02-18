859e01820ac7a15354e2569010587bf17afe8ba8
Erik Kotsiuba <ErikKotsiuba@udelphi.com>
2017-02-18 23:09:47 +0200

    changing li text

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-/*for(var i = 0; i < 10; i++) {
-	console.log("hello");
-// }*/
+// for(var i = 0; i < 10; i++) {
+// 	console.log("hello");
+// }
 
 // function Add(a, b) {
 // 	console.log(a + b);
-
 // 	return a + b;
 // }
 
 // var sum = Add(2, 2);
 
 var ul = document.querySelectorAll("li");
-console.log(ul);
+
+ul.forEach(function(element, index){
+	element.innerHTML = "element " + index;
+});
