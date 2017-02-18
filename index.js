cb97c141270c01ad5795947b72ac248539199ba9
Erik Kotsiuba <ErikKotsiuba@udelphi.com>
2017-02-18 23:09:26 +0200

    js

---
+++ index.js
@@ -0,0 +1,14 @@
+/*for(var i = 0; i < 10; i++) {
+	console.log("hello");
+// }*/
+
+// function Add(a, b) {
+// 	console.log(a + b);
+
+// 	return a + b;
+// }
+
+// var sum = Add(2, 2);
+
+var ul = document.querySelectorAll("li");
+console.log(ul);
